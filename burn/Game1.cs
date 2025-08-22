b0a84b4886116ea98366805cd0de45cbbed94c72
Adam Waggoner <adam.swaggoner@gmail.com>
2025-08-22 14:13:45 -0500

    feat: intial commit

---
+++ burn/Game1.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Peridot;
+
+namespace burn;
+
+public class Game1 : Core
+{
+
+    public Game1()
+    : base("burn", 720, 480, false)
+    {
+
+    }
+
+    protected override void Initialize()
+    {
+        // TODO: Add your initialization logic here
+
+        base.Initialize();
+    }
+
+    protected override void LoadContent()
+    {
+
+        // TODO: use this.Content to load your game content here
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+
+        // TODO: Add your update logic here
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+
+        // TODO: Add your drawing code here
+
+        base.Draw(gameTime);
+    }
+}
